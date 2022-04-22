41ca65392dd08aba21812b32056c2d5025d217be
Viktor <Rickeygame23@gmail.com>
2022-04-22 08:47:45 +0300

    warplay add

--- src/js/script.js
+++ src/js/script.js
@@ -5,10 +5,12 @@ const hamburger = document.querySelector('.hamburger'),
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
+    hamburger.classList.add('active');
 })
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+    hamburger.classList.remove('active');
 })
 
 const counters = document.querySelectorAll('.skills__block-counters'),
