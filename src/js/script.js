faecef362bde51b063d33d9c87811bc8704f1717
Viktor <Rickeygame23@gmail.com>
2022-04-26 21:32:38 +0300

    fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,25 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
+    menu_item = document.querySelectorAll('.menu__link a'),
     closeElem = document.querySelector('.menu__close');
 
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
     hamburger.classList.add('active');
-})
+});
+
+menu_item.forEach((e) => {
+  e.addEventListener('click', () => {
+        menu.classList.remove('active');
+        hamburger.classList.remove('active');      
+  })
+});
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
     hamburger.classList.remove('active');
-})
+});
 
 const counters = document.querySelectorAll('.skills__block-counters'),
     lines = document.querySelectorAll('.skills__block-scale span');
